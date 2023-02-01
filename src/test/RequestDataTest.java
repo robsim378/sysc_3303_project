658a6cc5104308167c5047ddb413c4410fbfdcbf
macbook <Khalidmerai@cmail.carleton.ca>
2023-02-01 16:57:34 -0500

    Unit tests and README

---
+++ src/test/RequestDataTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.Test;
+
+import sysc_3303_project.Direction;
+import sysc_3303_project.RequestData;
+
+public class RequestDataTest {
+
+    @Test
+    public void testGetRequestTime() {
+        LocalTime time = LocalTime.of(12, 0, 0);
+        RequestData request = new RequestData(time, 1, Direction.UP, 5);
+        assertEquals(time, request.getRequestTime());
+    }
+
+    @Test
+    public void testGetCurrentFloor() {
+        RequestData request = new RequestData(LocalTime.of(12, 0, 0), 1, Direction.UP, 5);
+        assertEquals(1, request.getCurrentFloor());
+    }
+
+    @Test
+    public void testGetDirection() {
+        RequestData request = new RequestData(LocalTime.of(12, 0, 0), 1, Direction.UP, 5);
+        assertEquals(Direction.UP, request.getDirection());
+    }
+
+    @Test
+    public void testGetDestinationFloor() {
+        RequestData request = new RequestData(LocalTime.of(12, 0, 0), 1, Direction.UP, 5);
+        assertEquals(5, request.getDestinationFloor());
+    }
+}
---
+++ src/test/SchedulerTest.java
@@ -0,0 +1,67 @@
+package test;
+
+
+import static org.junit.Assert.*;
+import java.time.LocalTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sysc_3303_project.Direction;
+import sysc_3303_project.RequestData;
+import sysc_3303_project.Scheduler;
+
+public class SchedulerTest {
+
+    private Scheduler scheduler;
+
+    @Before
+    public void setUp() {
+        scheduler = new Scheduler();
+    }
+
+    @Test
+    public void testHasRequests() {
+        assertFalse(scheduler.hasRequests());
+
+        LocalTime time = LocalTime.now();
+        scheduler.addRequest(new RequestData(time, 1, Direction.UP, 3));
+
+        assertTrue(scheduler.hasRequests());
+    }
+
+    @Test
+    public void testAddRequest() {
+        assertFalse(scheduler.hasRequests());
+
+        LocalTime time = LocalTime.now();
+        RequestData request = new RequestData(time, 1, Direction.UP, 3);
+        scheduler.addRequest(request);
+
+        assertTrue(scheduler.hasRequests());
+        assertEquals(request, scheduler.getRequest());
+    }
+
+    @Test
+    public void testHasResponses() {
+        assertFalse(scheduler.hasResponses());
+
+        LocalTime time = LocalTime.now();
+        RequestData response = new RequestData(time, 1, Direction.UP, 3);
+        scheduler.addResponse(response);
+
+        assertTrue(scheduler.hasResponses());
+    }
+
+    @Test
+    public void testAddResponse() {
+        assertFalse(scheduler.hasResponses());
+
+        LocalTime time = LocalTime.now();
+        RequestData response = new RequestData(time, 1, Direction.UP, 3);
+        scheduler.addResponse(response);
+
+        assertTrue(scheduler.hasResponses());
+        assertEquals(response, scheduler.getResponse());
+    }
+}
