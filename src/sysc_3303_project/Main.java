4660a59fbe897fdf1c19b456d58d1ad811ede68f
Robert Simionescu <robsim378@gmail.com>
2023-02-01 14:23:29 -0500

    Added Main

---
+++ src/sysc_3303_project/Main.java
@@ -0,0 +1,23 @@
+package sysc_3303_project;
+
+import java.io.File;
+
+/**
+ * @author Robert Simionescu
+ */
+public class Main {
+    public static void main(String[] args) {
+        Thread elevator, floorSystem;
+        Scheduler scheduler;
+
+        scheduler = new Scheduler();
+
+        elevator = new Thread(new Elevator(scheduler, 0), "Elevator");
+
+        String floorFilePath = new File("").getAbsolutePath() + "/resources/testing_examples";
+        floorSystem = new Thread(new FloorSystem(scheduler, floorFilePath), "FloorSystem");
+
+        elevator.start();
+        floorSystem.start();
+    }
+}
