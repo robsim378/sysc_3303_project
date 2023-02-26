98234bd3bc869ed8dc9e292ee9ab5328b47c3ba3
ianh6 <ian.a.holmes@sympatico.ca>
2023-02-26 12:00:41 -0500

    add doors closed state tests

--- src/module-info.java
+++ src/module-info.java
@@ -6,13 +6,14 @@
  *
  */
 module sysc_3303_project {
-	exports sysc_3303_project;
+    exports sysc_3303_project;
     exports sysc_3303_project.elevator_subsystem; //for reflection in unit tests
     exports test.common;
     exports test.elevator_subsystem;
+    exports test.elevator_subsystem.states;
     exports test.floor_subsystem;
     exports test.floor_subsystem.states;
     exports test.scheduler_subsystem;
     requires junit;
-	requires org.junit.jupiter.api;
+    requires org.junit.jupiter.api;
 }
