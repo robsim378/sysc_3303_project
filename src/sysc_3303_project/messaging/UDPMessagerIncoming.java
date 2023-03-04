764de9a60d15ef568912a1ead38c84c07b61e5ed
dwarflord1 <saggysar@gmail.com>
2023-03-04 14:03:41 -0500

    added runnable to methods

--- src/sysc_3303_project/messaging/UDPMessagerIncoming.java
+++ src/sysc_3303_project/messaging/UDPMessagerIncoming.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import sysc_3303_project.common.EventBuffer;
 
-public class UDPMessagerIncoming<T> {
+public class UDPMessagerIncoming<T> implements Runnable{
 	
 	private List<EventBuffer<T>> eventBuffers;
 	
 	public UDPMessagerIncoming(List<EventBuffer<T>> eventBuffers) {
 		this.eventBuffers = eventBuffers;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/sysc_3303_project/messaging/UDPMessagerOutgoing.java
+++ src/sysc_3303_project/messaging/UDPMessagerOutgoing.java
@@ -2,11 +2,17 @@ package sysc_3303_project.messaging;
 
 import sysc_3303_project.common.EventBuffer;
 
-public class UDPMessagerOutgoing {
+public class UDPMessagerOutgoing implements Runnable{
 
 	private EventBuffer<?> eventBuffer;
 	
 	public UDPMessagerOutgoing(EventBuffer<?> eventBuffer) {
 		this.eventBuffer = eventBuffer;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 }
