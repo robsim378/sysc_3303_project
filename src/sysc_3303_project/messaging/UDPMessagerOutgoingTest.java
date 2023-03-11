32940f48f035268df9e35f3ea0b2e832079fcd2e
macbook <Khalidmerai@cmail.carleton.ca>
2023-03-11 01:21:43 -0500

    Created test for UDPMessagerOutgoing

---
+++ src/sysc_3303_project/messaging/UDPMessagerOutgoingTest.java
@@ -0,0 +1,36 @@
+package sysc_3303_project.messaging;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sysc_3303_project.common.EventBuffer;
+import sysc_3303_project.common.Subsystem;
+import sysc_3303_project.common.SystemProperties;
+
+import static org.junit.Assert.assertEquals;
+
+public class UDPMessagerOutgoingTest {
+
+    private EventBuffer<TestEnum> eventBuffer;
+    private UDPMessagerOutgoing<TestEnum> udpMessagerOutgoing;
+
+    @Before
+    public void setUp() {
+        eventBuffer = new EventBuffer<>();
+        udpMessagerOutgoing = new UDPMessagerOutgoing<>(eventBuffer);
+    }
+
+    @Test
+    public void testGetPort() {
+        assertEquals(SystemProperties.ELEVATOR_PORT, udpMessagerOutgoing.getPort(Subsystem.ELEVATOR));
+        assertEquals(SystemProperties.FLOOR_PORT, udpMessagerOutgoing.getPort(Subsystem.FLOOR));
+        assertEquals(SystemProperties.SCHEDULER_PORT, udpMessagerOutgoing.getPort(Subsystem.SCHEDULER));
+        assertEquals(-1, udpMessagerOutgoing.getPort(null));
+    }
+
+    public enum TestEnum {
+        CONSTANT1,
+        CONSTANT2,
+        CONSTANT3
+    }
+}
