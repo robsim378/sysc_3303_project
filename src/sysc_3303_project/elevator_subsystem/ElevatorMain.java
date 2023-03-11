a88c38550743e57e491ee3c4e188a38d8b326fa5
ianh6 <ian.a.holmes@sympatico.ca>
2023-03-11 13:46:10 -0500

    fix main

--- src/sysc_3303_project/elevator_subsystem/ElevatorMain.java
+++ src/sysc_3303_project/elevator_subsystem/ElevatorMain.java
@@ -6,26 +6,33 @@ import sysc_3303_project.messaging.UDPMessagerIncoming;
 import sysc_3303_project.messaging.UDPMessagerOutgoing;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class ElevatorMain {
 
     public static void main(String[] args) {
+        EventBuffer<Enum<?>> outputBuffer = new EventBuffer<>();
+        List<EventBuffer<ElevatorEventType>> inputBuffersList = new LinkedList<>();
+        List<Elevator> elevators = new LinkedList<>();
 
         for (int i = 0; i < SystemProperties.MAX_ELEVATOR_NUMBER; i++) {
-            EventBuffer<Enum<?>> outputBuffer = new EventBuffer<>();
             EventBuffer<ElevatorEventType> inputBuffer = new EventBuffer<>();
 
             Elevator elevator = new Elevator(outputBuffer, inputBuffer, i);
 
-            UDPMessagerOutgoing out = new UDPMessagerOutgoing(outputBuffer);
-            UDPMessagerIncoming<ElevatorEventType> in = new UDPMessagerIncoming<>(new LinkedList<>() {{add(inputBuffer);}}, SystemProperties.SCHEDULER_PORT);
+            elevators.add(elevator);
+        }
+        UDPMessagerOutgoing out = new UDPMessagerOutgoing(outputBuffer);
+        UDPMessagerIncoming<ElevatorEventType> in = new UDPMessagerIncoming<>(inputBuffersList, SystemProperties.SCHEDULER_PORT);
 
-            Thread elevatorThread = new Thread(elevator);
-            Thread outUdpThread = new Thread(out);
-            Thread inUdpThread = new Thread(in);
+        Thread outUdpThread = new Thread(out);
+        Thread inUdpThread = new Thread(in);
+        Thread elevatorThread;
 
-            outUdpThread.start();
-            inUdpThread.start();
+        outUdpThread.start();
+        inUdpThread.start();
+        for (Elevator e : elevators) {
+            elevatorThread = new Thread(e);
             elevatorThread.start();
         }
     }
