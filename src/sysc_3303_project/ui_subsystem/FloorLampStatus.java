803456bbd64547165458775ff9412b13f2ce9e29
Andrei Popescu <apopescu486@gmail.com>
2023-03-31 15:10:30 -0400

    Implemented GuiModel to store data and allow View access

---
+++ src/sysc_3303_project/ui_subsystem/FloorLampStatus.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package sysc_3303_project.ui_subsystem;
+
+import sysc_3303_project.common.Direction;
+
+/**
+ * @author Andrei Popescu
+ *
+ */
+public class FloorLampStatus {
+	
+	private final Direction direction;
+	private final boolean status;
+	
+	public FloorLampStatus(Direction direction, boolean status) {
+		if (direction == null) {
+			throw new IllegalArgumentException("Floor lamp direction cannot be null!");
+		}
+		this.direction = direction;
+		this.status = status;
+	}
+	
+	public Direction getDirection() {
+		return direction;
+	}
+	
+	public boolean getStatus() {
+		return status;
+	}
+}
