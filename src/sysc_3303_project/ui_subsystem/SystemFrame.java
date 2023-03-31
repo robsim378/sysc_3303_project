74d136155e50cdfc60352aa66b360381ca10b36d
ianh6 <ian.a.holmes@sympatico.ca>
2023-03-31 15:31:05 -0400

    add view

---
+++ src/sysc_3303_project/ui_subsystem/SystemFrame.java
@@ -0,0 +1,13 @@
+package sysc_3303_project.ui_subsystem;
+
+public class SystemFrame implements GuiView {
+
+
+    public void updateFloorPanel(int floorID) {
+
+    }
+
+    public void updateElevatorPanel(int elevatorID) {
+
+    }
+}
