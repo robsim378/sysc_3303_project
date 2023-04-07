d4d6bed66d28a792b1717931a318cb92c3bf2b08
Khalidmerai <98438415+Khalidmerai@users.noreply.github.com>
2023-04-07 16:54:01 -0400

    ElevatorPanel test

---
+++ ElevatorPanelTest.java
@@ -0,0 +1,27 @@
+package sysc_3303_project.gui_subsystem.view;
+
+import org.junit.jupiter.api.Test;
+import sysc_3303_project.gui_subsystem.model.SystemModel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElevatorPanelTest {
+
+    @Test
+    void updatePanel() {
+        // Create a valid SystemModel instance
+        SystemModel model = new SystemModel();
+        model.setCurrentFloor(5);
+        model.setDoorStatus(true);
+
+        // Create a new ElevatorPanel instance
+        ElevatorPanel panel = new ElevatorPanel(1);
+
+        // Call the updatePanel() method with the valid SystemModel instance
+        panel.updatePanel(model);
+
+        // Check that the panel's floor label has been updated correctly
+        assertEquals("5", panel.getFloorLabel().getText(), "Panel floor label should be updated to '5'");
+    }
+
+}
