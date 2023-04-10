2c8a8e84672f3f126dccf07f5953e7803cedc13e
Andrei Popescu <apopescu486@gmail.com>
2023-04-09 23:25:54 -0400

    Fixed module-info from performance commits

--- src/module-info.java
+++ src/module-info.java
@@ -10,12 +10,13 @@ module sysc_3303_project {
     exports sysc_3303_project.common;
     exports sysc_3303_project.common.configuration;
     exports sysc_3303_project.common.events;
-//    exports test.gui_subsystem;
-//    exports test.messaging;
-//    exports test.common;
-//    exports test.elevator_subsystem;
-//    exports test.scheduler_subsystem;
-//    exports test.floor_subsystem;
+    // needed for reflection to work in tests
+    exports test.gui_subsystem;
+    exports test.messaging;
+    exports test.common;
+    exports test.elevator_subsystem;
+    exports test.scheduler_subsystem;
+    exports test.floor_subsystem;
 
     requires junit;
     requires org.junit.jupiter.api;
